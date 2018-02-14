2602499add2617de681ff0d7ef6b3c599c7a41bf
Jędrzej Ostrowski <jedrzej.ostrowski@gfactor.pl>
2018-02-14 15:28:07 +0100

    Redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook} from "../actions/index";
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li className="list-group-item">{book.title}</li>
+                <li
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title}
+                    className="list-group-item">
+                    {book.title}
+                </li>
             );
         });
     }
 
     render() {
         return(
-            <ul className="list-group col-sm-4">
-                {this.renderList()}
-            </ul>
+            <div>
+                <ul className="list-group col-sm-4">
+                    {this.renderList()}
+                </ul>
+                {this.props.selectBook.title} elo
+            </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+      books: state.books
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
